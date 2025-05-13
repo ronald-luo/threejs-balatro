c0a4b27adca4756721e5002a3cfb51c24e5b574c
ronald-luo <r.luo.180@gmail.com>
2025-05-12 23:22:55 -0400

    add shaders as plain text

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  plugins: [glsl()],
+  assetsInclude: ["**/*.glsl"],
   server: {
     open: true,
   },
